268505c3c9f84b837084ee84b233be3597349857
drregg6 <dave.r.regg@gmail.com>
2023-04-09 17:03:43 -0400

    WeatherApp

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dave Regg on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
